b2c7e62c1cbfd06daef03d1b57288c326d342979
Frankie <fj9372@rit.edu>
2024-03-13 14:35:58 -0400

    created react and started database code

---
+++ src/python/api/utils.py
@@ -0,0 +1,105 @@
+import psycopg2
+import yaml
+import os
+from sshtunnel import SSHTunnelForwarder
+
+def connect():
+    config = {}
+    yml_path = os.path.join(os.path.dirname(__file__), '../db.yml')
+    with open(yml_path, 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    try:
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=config['user'],
+                                ssh_password=config['password'],
+                                remote_bind_address=('127.0.0.1', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            conn = psycopg2.connect(database=config['database'],
+                                user=config['user'],
+                                password=config['password'],
+                                host=config['host'],
+                                port=server.local_bind_port)
+            curs = conn.cursor()
+            print("Database connection established")
+            curs.execute("SELECT * FROM author")
+            rows = curs.fetchall()
+            print(rows)
+            conn.close()
+    except Exception as e:
+        print("Connection failed", e)
+
+def exec_get_one(sql, args={}):
+    config = {}
+    yml_path = os.path.join(os.path.dirname(__file__), '../db.yml')
+    with open(yml_path, 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    try:
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=config['user'],
+                                ssh_password=config['password'],
+                                remote_bind_address=('127.0.0.1', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            conn = psycopg2.connect(database=config['database'],
+                                user=config['user'],
+                                password=config['password'],
+                                host=config['host'],
+                                port=server.local_bind_port)
+            cur = conn.cursor()
+            cur.execute(sql, args)
+            one = cur.fetchone()
+            conn.close()
+            return one
+    except Exception as e:
+        print("Connection failed", e)
+
+def exec_get_all(sql, args={}):
+    config = {}
+    yml_path = os.path.join(os.path.dirname(__file__), '../db.yml')
+    with open(yml_path, 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    try:
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=config['user'],
+                                ssh_password=config['password'],
+                                remote_bind_address=('127.0.0.1', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            conn = psycopg2.connect(database=config['database'],
+                                user=config['user'],
+                                password=config['password'],
+                                host=config['host'],
+                                port=server.local_bind_port)
+            cur = conn.cursor()
+            cur.execute(sql, args)
+            all = cur.fetchall()
+            conn.close()
+            return all
+    except Exception as e:
+        print("Connection failed", e)
+
+def exec_commit(sql, args={}):
+    config = {}
+    yml_path = os.path.join(os.path.dirname(__file__), '../db.yml')
+    with open(yml_path, 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    try:
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=config['user'],
+                                ssh_password=config['password'],
+                                remote_bind_address=('127.0.0.1', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+            conn = psycopg2.connect(database=config['database'],
+                                user=config['user'],
+                                password=config['password'],
+                                host=config['host'],
+                                port=server.local_bind_port)
+            cur = conn.cursor()
+            result = cur.execute(sql, args)
+            conn.commit()
+            conn.close()
+            return result
+    except Exception as e:
+        print("Connection failed", e)
---
+++ src/python/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+from api.utils import *
+
+app = Flask(__name__) #create Flask instance
+CORS(app) #Enable CORS on Flask server to work with Nodejs pages
+api = Api(app) #api router
+
+if __name__ == '__main__':
+    print("Loading db")
+    print("Starting flask")
+    app.run(debug=True), #starts Flask
+
+
