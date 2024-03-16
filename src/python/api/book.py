5da1478d03481e8ae1a39f83cfd76b0418dd05be
rytherlm <111656411+arr9180@users.noreply.github.com>
2024-03-15 21:12:46 -0400

    Worked on the api book stuff

--- src/python/api/book.py
+++ src/python/api/book.py
@@ -1,19 +1,40 @@
 from flask_restful import Resource
-
-from flask_restful import request
 from flask_restful import reqparse
 from .utils import *
 
+
 class Book(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('BookID', type=int, store_missing=False)
+    parser.add_argument('Title', store_missing=False)
+    parser.add_argument('ReleaseDate', store_missing=False)
+    parser.add_argument('Length', type=int, store_missing=False)
+    parser.add_argument('Audience', store_missing=False)
+
     def get(self):
-        return
-    
+        args = self.parser.parse_args(strict=False) 
+        if 'BookID' in args:
+            sql = "SELECT * FROM Book WHERE BookID = %s"
+            result = exec_get_one(sql, (args['BookID'],))
+        else:
+            sql = "SELECT * FROM Book"
+            result = exec_get_all(sql)
+        return result if result else {'message': 'Book not found'}, 404
+
     def put(self):
-        return
+        args = self.parser.parse_args()
+        sql = "UPDATE Book SET Title = %s, ReleaseDate = %s, Length = %s, Audience = %s WHERE BookID = %s"
+        exec_commit(sql, (args['Title'], args['ReleaseDate'], args['Length'], args['Audience'], args['BookID']))
+        return {'message': 'Book updated successfully'}
 
-    def post(self):   
-        return
-    
+    def post(self):
+        args = self.parser.parse_args()
+        sql = "INSERT INTO Book (Title, ReleaseDate, Length, Audience) VALUES (%s, %s, %s, %s)"
+        exec_commit(sql, (args['Title'], args['ReleaseDate'], args['Length'], args['Audience']))
+        return {'message': 'Book created successfully'}
+
     def delete(self):
-        return
-
+        args = self.parser.parse_args()
+        sql = "DELETE FROM Book WHERE BookID = %s"
+        exec_commit(sql, (args['BookID'],))
+        return {'message': 'Book deleted successfully'}
--- src/python/server.py
+++ src/python/server.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
-
+from api.book import Book
 from api.utils import *
 
 app = Flask(__name__) #create Flask instance
 CORS(app) #Enable CORS on Flask server to work with Nodejs pages
 api = Api(app) #api router
+
+api.add_resource(Book, '/book')  
 
 if __name__ == '__main__':
     print("Loading db")
