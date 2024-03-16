a11cc6aa8cda438d6e6534d39c6789682d16a110
rytherlm <111656411+arr9180@users.noreply.github.com>
2024-03-16 18:22:30 -0400

    Working on more api stuff

--- src/python/api/publisher.py
+++ src/python/api/publisher.py
@@ -5,15 +5,57 @@ from flask_restful import reqparse
 from .utils import *
 
 class Publisher(Resource):
-    def get(self):
-        return
-    
+    def get(self, contributor_id=None):
+        if contributor_id is not None:
+            sql = "SELECT * FROM Publisher WHERE ContributorID = %s"
+            result = exec_get_one(sql, (contributor_id,))
+            if result:
+                return result, 200
+            else:
+                return {}, 404
+        else:
+            sql = "SELECT * FROM Publisher"
+            result = exec_get_all(sql)
+            if result:
+                return result, 200
+            return [], 200
+
     def put(self):
-        return
+        parser = reqparse.RequestParser()
+        parser.add_argument('ContributorID', type=int, required=True)
+        parser.add_argument('FirstName', required=True)
+        parser.add_argument('LastName', required=True)
+        args = parser.parse_args()
 
-    def post(self):   
-        return
-    
+        sql = "UPDATE Contributor SET FirstName = %s, LastName = %s WHERE ContributorID = %s"
+        if exec_commit(sql, (args['FirstName'], args['LastName'], args['ContributorID'])):
+            return 200
+        return 404
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('FirstName', required=True)
+        parser.add_argument('LastName', required=True)
+        args = parser.parse_args()
+
+        sql_insert_contributor = "INSERT INTO Contributor (FirstName, LastName) VALUES (%s, %s) RETURNING ContributorID"
+        result = exec_commit(sql_insert_contributor, (args['FirstName'], args['LastName']))
+        
+        if result:
+            contributor_id = result
+            sql_insert_publisher = "INSERT INTO Publisher (ContributorID) VALUES (%s)"
+            if exec_commit(sql_insert_publisher, (contributor_id,)):
+                return 201
+        return 404
+
     def delete(self):
-        return
-
+        contributor_id = request.args.get('ContributorID', type=int)
+        if contributor_id:
+            sql_delete_publisher = "DELETE FROM Publisher WHERE ContributorID = %s"
+            exec_commit(sql_delete_publisher, (contributor_id,))
+            
+            sql_delete_contributor = "DELETE FROM Contributor WHERE ContributorID = %s"
+            if exec_commit(sql_delete_contributor, (contributor_id,)):
+                return 200
+            return 404
+        return 400
