3f124a7cc97534531cfa80895949753ce5510081
rytherlm <111656411+arr9180@users.noreply.github.com>
2024-04-04 11:43:38 -0400

    more stuff

---
+++ src/react-library/src/components/profile.js
@@ -0,0 +1,7 @@
+
+// • The application provides an user profile functionality that displays the following infor-
+// mation:
+// – The number of collections the user has
+// – The number of users followed by this user
+// – The number of users this user is following
+// – Their top 10 books (by highest rating, most read, or combination)
---
+++ src/react-library/src/components/recommend.js
@@ -0,0 +1,8 @@
+
+// • The application must provide a book recommendation system with the following op-
+// tions:
+// – The top 20 most popular books in the last 90 days (rolling)
+// – The top 20 most popular books among my followers
+// – The top 5 new releases of the month (calendar month)
+// – For you: Recommend books to read to based on your read history (e.g. genre,
+// author, rating) and the read history of similar users
